d20074298b4e3f74da25301e333b4c214184b233
minhsk96 <tminhsk@gmail.com>
2019-10-08 16:23:07 +0700

    .

--- link.c
+++ link.c
@@ -20,17 +20,19 @@ int main(int argc, char *argv[]){
     char *lastslash;
     char* name;
     char cwd[PATH_MAX];
-
+    char link[500];
     CURL *handle = curl_easy_init();
     CURLcode res;
-    FILE *file, *f;
+    FILE *file, *f, *f1;
+    f1 = fopen(argv[2],"r");
+    fscanf(f1,"%s",link);
 
-    lastslash = strrchr(argv[1], '/');
-    name = lastslash ? lastslash + 1 : argv[1];
+    lastslash = strrchr(link, '/');
+    name = lastslash ? lastslash + 1 : link;
     //printf("\nURL: %s\n", argv[1]);
     //printf("Filename: %s\n", name);
 
-    curl_easy_setopt(handle, CURLOPT_URL, argv[1]);
+    curl_easy_setopt(handle, CURLOPT_URL, link);
     file = fopen(name, "wb");       
     curl_easy_setopt(handle, CURLOPT_WRITEDATA, file);
     //curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data); 
@@ -40,12 +42,11 @@ int main(int argc, char *argv[]){
     fseek(file, 0L, SEEK_END); // get file size 
     int sz = ftell(file); // file size = sz 
 
-    fclose(file);
     curl_easy_cleanup   (handle);
 
     f = fopen("status.txt", "w+");
     getcwd(cwd, sizeof(cwd));
-    fprintf(f,"link download: %s\n",argv[1]);
+    fprintf(f,"link download: %s\n",link);
     fprintf(f,"link path local: %s\n",cwd);
     if(res==0 && sz !=0) {
         fprintf(f,"status: success");
@@ -53,7 +54,8 @@ int main(int argc, char *argv[]){
     else{
         fprintf(f,"status: false");
     }
+    fclose(file);
     fclose(f);
+    fclose(f1);
     return 0;
 }
-
