85af4e076a7158ef6c4edf6de567e5c6fb9debf0
minhsk96 <tminhsk@gmail.com>
2019-10-10 08:53:55 +0700

    download multi files

--- link.c
+++ link.c
@@ -12,49 +12,54 @@ size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     return written;
 }
 
+
 int main(int argc, char *argv[]){
 
-  
-    //"https://codeload.github.com/minhsk96/nt/zip/master"};
     int n = 0, k = 0;
     char *lastslash;
     char* name;
     char cwd[PATH_MAX];
-    char link[500];
+    char chunk[200];
     CURL *handle = curl_easy_init();
     CURLcode res;
-    FILE *file, *f, *f1;
-    f1 = fopen(argv[2],"r");
-    fscanf(f1,"%s",link);
+    FILE *f, *f1;
+    f1 = fopen(argv[1],"r");
+    f = fopen("status.txt", "w");
+    
+    //while(fgets(chunk, sizeof(chunk), fp) != NULL) {
+    while (fscanf(f1, "%s", chunk) != EOF) {
+        FILE *file;
+        lastslash = strrchr(chunk, '/');
+        name = lastslash ? lastslash + 1 : chunk;
 
-    lastslash = strrchr(link, '/');
-    name = lastslash ? lastslash + 1 : link;
-    //printf("\nURL: %s\n", argv[1]);
-    //printf("Filename: %s\n", name);
+        curl_easy_setopt(handle, CURLOPT_URL, chunk);
+        file = fopen(name, "wb");   
 
-    curl_easy_setopt(handle, CURLOPT_URL, link);
-    file = fopen(name, "wb");       
-    curl_easy_setopt(handle, CURLOPT_WRITEDATA, file);
-    //curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data); 
-    res = curl_easy_perform(handle);
-    //printf("%d",res);
+        curl_easy_setopt(handle, CURLOPT_WRITEDATA, file);
+        //curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data);
+        res = curl_easy_perform(handle);
+        fprintf(f,"link download: %s\n", chunk);
+        printf("download file name: %s\n", name);
+        printf("%d\n",res);
+        
+        fseek(file, 0L, SEEK_END); // get file size 
+        int sz = ftell(file); // file size = sz 
 
-    fseek(file, 0L, SEEK_END); // get file size 
-    int sz = ftell(file); // file size = sz 
+        getcwd(cwd, sizeof(cwd));
+        fprintf(f,"link path local: %s\n", cwd);
 
-    curl_easy_cleanup   (handle);
+        if(res==0 && sz !=0) {
+            fprintf(f,"status: success");
+        }
+        else{
+            fprintf(f,"status: false");
+        }
 
-    f = fopen("status.txt", "w+");
-    getcwd(cwd, sizeof(cwd));
-    fprintf(f,"link download: %s\n",link);
-    fprintf(f,"link path local: %s\n",cwd);
-    if(res==0 && sz !=0) {
-        fprintf(f,"status: success");
+        fprintf(f,"\n============================\n\n");
+        fclose(file);
+        
     }
-    else{
-        fprintf(f,"status: false");
-    }
-    fclose(file);
+    curl_easy_cleanup (handle);
     fclose(f);
     fclose(f1);
     return 0;
